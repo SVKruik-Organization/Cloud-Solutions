c211a8f768d7a4a034c27a6b674232656f3eabc5
Stefan Kruik <s.kruik@icloud.com>
2024-04-06 12:27:09 +0200

    Pipeline Test 5

--- cloud-solutions-pipeline/src/App.vue
+++ cloud-solutions-pipeline/src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-      <h2>Pipeline Test 4</h2>
+      <h2>Pipeline Test 5</h2>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
